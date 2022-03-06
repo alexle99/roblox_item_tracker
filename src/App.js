6dcfea378a4886d86542204386d286005a99f428
Alexander Le <alexanvl@uci.edu>
2022-03-06 04:34:57 -0800

    asdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const AccountBox = ({ text }) => {
+  const handleClick = () => {
+    console.log("ASDF " + text);
+  };
+  return (
+    <div className="Account-box" onClick={handleClick}>
+      <p>{text}</p>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>Accounts</h1>
+      </div>
+      <div className="Main-content">
+        <AccountBox text="asdf" />
+        <AccountBox text="zxcv" />
+        <AccountBox text="qwer" />
+        <AccountBox text="wert" />
+        <AccountBox text="sdfg" />
+        <AccountBox text="xcvb" />
+        <AccountBox text="cvbn" />
+        <AccountBox text="zxcv" />
+        <AccountBox text="qwer" />
+        <AccountBox text="wert" />
+        <AccountBox text="sdfg" />
+        <AccountBox text="xcvb" />
+        <AccountBox text="cvbn" />
+        <AccountBox text="zxcv" />
+      </div>
     </div>
   );
 }
